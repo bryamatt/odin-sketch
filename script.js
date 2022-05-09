5f985592c1adb27a7ed67611d434cbc89e0cb22d
Matt <bryamatt@gmail.com>
2022-05-08 21:34:20 -0400

    set up grid

---
+++ script.js
@@ -0,0 +1,19 @@
+function drawGrid(width) {
+    for (let i = 0; i < width; i++) {
+       
+       let iDiv = document.createElement('div');
+       iDiv.id = 'row' + i;
+       iDiv.className = 'pixel-row';
+
+       for (let j = 0; j < width; j++) {
+           let jDiv = document.createElement('div');
+           jDiv.id = 'col' + j;
+           jDiv.className = 'pixel-col';
+           iDiv.appendChild(jDiv);
+       }
+
+       document.getElementById('canvas').appendChild(iDiv);
+    }
+}
+
+drawGrid(16);
