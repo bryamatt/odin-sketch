3441ce33511aa4642071af775be33d2b69c26771
Matt <bryamatt@gmail.com>
2022-05-09 21:44:26 -0400

    can change number of squares

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function drawGrid(width) {
+function drawGrid(width) 
+{
+    const canvas = document.getElementById('canvas');
+
+    canvas.innerHTML = "";
+
     for (let i = 0; i < width; i++) {
        
        let iDiv = document.createElement('div');
@@ -9,20 +14,37 @@ function drawGrid(width) {
            let jDiv = document.createElement('div');
            jDiv.id = 'col' + j;
            jDiv.className = 'pixel-col';
+           jDiv.style.backgroundColor= 'rgb(255,255,255)';
            iDiv.appendChild(jDiv);
        }
 
-       document.getElementById('canvas').appendChild(iDiv);
+       canvas.appendChild(iDiv);
     }
+    const pixels = document.querySelectorAll('.pixel-col');
+
+    pixels.forEach(pixel => pixel.addEventListener('mouseenter', changeColour));
 }
 
 function changeColour(pixel) {
-    this.classList.add('darken');
+    //this.classList.add('darken');
+    const currColour = Number(this.style.backgroundColor.slice(4,7).split(',').join(''));
+    const newColour = currColour - 26;
+    this.style.backgroundColor = `rgb(${newColour},${newColour},${newColour})`;
+}
+
+function changeGridSize() {
+    let gridSize = Number(prompt("Enter new grid size between 1 and 100"))
+    if (gridSize > 0 && gridSize <= 100) {
+        drawGrid(gridSize);
+    }
+    else {
+        alert("Not a valid input");
+    }
 }
 
 drawGrid(16);
 
-const pixels = document.querySelectorAll('.pixel-col');
 
-pixels.forEach(pixel => pixel.addEventListener('mouseenter', changeColour));
+const button = document.getElementById('numSquares');
 
+button.addEventListener('click', changeGridSize);
